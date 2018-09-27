44d5f28102091c5e2d0706e71cb04afc11d1d8e8
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-09-27 08:11:19 -0700

    init the edit component

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is from Edit page.
-    </div>
-); 
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Editing Expense with id of {props.match.params.id}!
+        </div>
+    )
+}; 
 
 export default EditExpensePage;
