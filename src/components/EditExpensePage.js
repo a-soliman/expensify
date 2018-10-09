6dbb86b91009c863cb9fb916004e7a60caae66a5
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-09 15:20:38 -0700

    reused the add form to make edit expense functionality

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { editExpense } from '../actions/expenses';
+import ExpenseForm from './ExpenseForm';
 
 const EditExpensePage = (props) => {
-    console.log(props);
+    console.log(props.expense);
     return (
         <div>
             Editing Expense with id of {props.match.params.id}!
+            <ExpenseForm 
+                expense={props.expense} 
+                onSubmit={(expense) => {
+                    props.dispatch(editExpense(props.match.params.id, expense));
+                    props.history.push('/');
+                }}    
+            />
         </div>
     )
-}; 
+};
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+    }
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
     <div>
-        <h1>{description}</h1>
+        <Link to={`/edit/${id}`}>
+            <h1>{description}</h1>
+        </Link>
         <p>{amount}</p>
         <p>{moment(createdAt).format("MMM DD, YYYY")}</p>
         <button onClick={() => {
