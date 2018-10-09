1b58c8ab2c8cccf6a8591d73617f4eb25a859bc5
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-09 10:14:22 -0700

    more with higherOrderComponents

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -22,16 +22,29 @@ const Info = (props) => (
         <p>The info is: {props.info}</p>
     </div>
 );
-
-const withPrivateMessage = (WrappedComponent) => {
+const requireAuthentication = (WrappedComponent) => {
     return (props) => (
         <div>
-            {props.isAdmin && <p>These are private information. Please DO NOT share!</p>}
-            <WrappedComponent {...props} />
+            {!props.isAuthenticated && <p>Please Sign in to view the info..</p>}
+            {props.isAuthenticated &&
+                <WrappedComponent {...props} />
+            }
         </div>
     );
 };
+const AuthInfo = requireAuthentication(Info);
 
-const AdminInfo = withPrivateMessage(Info);
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="These are the info" />, document.querySelector('#app'));
 
-ReactDOM.render(<AdminInfo isAdmin={true} info="There are the information!" />, document.querySelector('#app'));
+// const withPrivateMessage = (WrappedComponent) => {
+//     return (props) => (
+//         <div>
+//             {props.isAdmin && <p>These are private information. Please DO NOT share!</p>}
+//             <WrappedComponent {...props} />
+//         </div>
+//     );
+// };
+
+// const AdminInfo = withPrivateMessage(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="There are the information!" />, document.querySelector('#app'));
