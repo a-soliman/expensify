3e7d915b200ac1cbd15bee09c6a65febb3df6b16
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-09 14:40:42 -0700

    formated the output of the time stamp using moment()

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
     <div>
         <h1>{description}</h1>
         <p>{amount}</p>
-        <p>{createdAt}</p>
+        <p>{moment(createdAt).format("MMM DD, YYYY")}</p>
         <button onClick={() => {
             dispatch(removeExpense({id}));
         }}>Remove</button>
