55907e611ab9416b4b7b6c1a4d3243540bee1a77
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-08 17:20:53 -0700

    completed the playfround for reduxStore

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -36,10 +36,31 @@ const editExpense = ( id, updates) => ({
 });
 // + FILTER
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
 // SORT_BY_DATE
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+})
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
 // SET_START_DATE
+const setStartDate = (date) => ({
+    type: 'SET_START_DATE',
+    date
+});
+
 // SET_END_DATE
+const setEndDate = (date) => ({
+    type: 'SET_END_DATE',
+    date
+});
 
 // EXPENSES REDUCER
 const expensesReducerDefaultState = [];
@@ -80,6 +101,36 @@ const filtersReducerDefaultState = {
 // FILTERS REDUCER
 const filtersReducer = ( state = filtersReducerDefaultState, action ) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
+        
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            };
+
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            };
+        
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.date
+            }
+        
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.date
+            }
+
         default:
             return state;
     }
@@ -104,6 +155,21 @@ const expenseTwo = store.dispatch(addExpense({description: 'Education', amount: 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500}));
+
+store.dispatch(setTextFilter('rent'));
+
+store.dispatch(sortByAmount());
+
+store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(125));
+
+store.dispatch(setStartDate());
+
+store.dispatch(setEndDate(125));
+
+store.dispatch(setEndDate());
+
 
 const demoState = {
     expenses: [{
