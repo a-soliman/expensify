6b22ec5dc4bd3bb59c743e618c52bd6227a71647
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-09 09:41:28 -0700

    imported the new created actions, store and methods - tested the app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppRouter from './routers/AppRouter'
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses'; 
+
 import 'normalize-scss/sass/_normalize.scss';
 import './styles/styles.scss';
 
+const store = configureStore();
 
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+const expenseOne = store.dispatch(addExpense({description: 'Water bill', amount: 125, createdAt: 200}));
+const expenseTwo = store.dispatch(addExpense({description: 'Gas bill', amount: 100, createdAt: 201}));
+
+store.dispatch(setTextFilter('gas'));
 
 const appContainer = document.querySelector('#app');
 ReactDOM.render(<AppRouter />, appContainer);
