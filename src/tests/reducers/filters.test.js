9b5acc500574291e7d937913518fb79b6f8ab038
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-18 07:16:30 -0700

    wrote test case to make sure the the @@init is providing the correct default values

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -3,5 +3,15 @@ import moment from 'moment';
 
 describe('FiltersReducer', () => {
     
+    test('Should setup default filter values', () => {
+        const state = filtersReducer(undefined, { type: '@@INIT' });
+        expect(state).toEqual({
+            text: '',
+            sortBy: 'date',
+            startDate: moment().startOf('month'),
+            endDate:  moment().endOf('month')
+        });
+    });
+
     
 });
