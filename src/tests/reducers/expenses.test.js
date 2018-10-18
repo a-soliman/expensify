b1d6c79506d904a01775eb13d89254adedf6a105
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-18 07:23:10 -0700

    testing the default state @init

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -2,4 +2,9 @@ import expensesReducer from '../../reducers/expenses';
 
 describe('ExpensesReducer', () => {
     
+    test('Should set the default state', () => {
+        const state = expensesReducer(undefined, { type: '@@INIT'} );
+        expect(state).toEqual([]);
+    });
+
 });
