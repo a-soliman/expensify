3aff36b88b7bd107e3ce9f608dca9ce5eef7af16
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-18 07:46:13 -0700

    testing removeExpense

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,7 +1,31 @@
 import expensesReducer from '../../reducers/expenses';
+import { stat } from 'fs';
 
 describe('ExpensesReducer', () => {
-    
+    const expenses = [
+        {
+            id: '1',
+            description: 'expense 1', 
+            note: '', 
+            amount: 0, 
+            createdAt: 0
+        },
+        {
+            id: '2',
+            description: 'expense 2', 
+            note: '', 
+            amount: 0, 
+            createdAt: 0
+        },
+        {
+            id: '3',
+            description: 'expense 3', 
+            note: '', 
+            amount: 0, 
+            createdAt: 0
+        }
+    ];
+
     test('Should set the default state', () => {
         const state = expensesReducer(undefined, { type: '@@INIT'} );
         expect(state).toEqual([]);
@@ -14,12 +38,22 @@ describe('ExpensesReducer', () => {
                 title: 'test'
             }
         };
-        const state = expensesReducer(undefined, action);
+        const state = expensesReducer(expenses, action);
         expect(state).toEqual([
+            ...expenses,
             { ...action.expense }
         ]);
     });
 
-    
+    test('Should remove expense', () => {
+        const action = {
+            type: 'REMOVE_EXPENSE',
+            id: '1'
+        };
+        const state = expensesReducer(expenses, action);
+        expect(state).toEqual([expenses[1], expenses[2]]);
+    });
+
+
 
 });
