d0cbcfb5b42de2abacdcdcd085322047830d8d35
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-19 07:29:46 -0700

    wrote test cases for onChange amount and description

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -15,4 +15,29 @@ describe('ExpeseForm Component', () => {
         const wrapper = shallow(<ExpenseForm expense={expense} />);
         expect(wrapper).toMatchSnapshot();
     });
+
+    test('Should render error for invalid form submision', () => {
+        const wrapper = shallow(<ExpenseForm />);
+        wrapper.find('form').simulate('submit', {preventDefault: () => {}});
+        expect(wrapper.state('error').length).toBeGreaterThan(0);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Should set description on input change', () => {
+        const value = 'new description';
+        const wrapper = shallow(<ExpenseForm />);
+        wrapper.find('input').at([0]).simulate('change', {target : { value }});
+        expect(wrapper.state('description')).toBe(value);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Should set amount on input change', () => {
+        const value = '555.00';
+        const wrapper = shallow(<ExpenseForm />);
+        wrapper.find('input').at([1]).simulate('change', {
+            target: { value }
+        });
+        expect(wrapper.state('amount')).toBe(value);
+        expect(wrapper).toMatchSnapshot();
+    });
 });
