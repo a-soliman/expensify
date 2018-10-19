a3f4162a22777da422d9586d7d15ac1293a319db
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-19 07:08:54 -0700

    testing expenseform component rendering with data

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
+import expenses from '../fixtures/expenses';
 
 describe('ExpeseForm Component', () => {
-    test('Should render ExpenseForm', () => {
+
+    test('Should render ExpenseForm correctly', () => {
         const wrapper = shallow(<ExpenseForm />);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Should render ExpenseForm with expense data', () => {
+        const expense = expenses[0];
+        const wrapper = shallow(<ExpenseForm expense={expense} />);
         expect(wrapper).toMatchSnapshot();
     });
 });
