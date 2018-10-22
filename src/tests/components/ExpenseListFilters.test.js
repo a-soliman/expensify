afbd4b41dc078020ee04a587a2148d5123ec9c23
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-22 07:16:54 -0700

    snapshot test case for ExpenseList Component

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+import { filters, altFilters } from '../fixtures/filters';
 
 describe('ExpenseListFilters Component', () => {
-    
+    let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+    beforeEach(() => {
+        setTextFilter = jest.fn();
+        sortByDate = jest.fn();
+        sortByAmount = jest.fn();
+        setStartDate = jest.fn();
+        setEndDate = jest.fn();
+        wrapper = shallow(
+            <ExpenseListFilters 
+                filters = {filters}
+                setTextFilter = {setTextFilter}
+                sortByDate = {sortByDate}
+                sortByAmount = {sortByAmount}
+                setStartDate = {setStartDate}
+                setEndDate = {setEndDate}
+            />
+        );
+    });
+
+    test('Should render ExpenseListFilters component corectly', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
 });
