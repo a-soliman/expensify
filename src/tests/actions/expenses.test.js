e56c28dace5bf7618adc6965e49d8bb85158581d
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-11-04 09:25:13 -0800

    added an action for set_expenses

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,18 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { addExpense, startAddExpense, removeExpense, editExpense } from '../../actions/expenses';
+import { addExpense, startAddExpense, removeExpense, editExpense, setExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) => {
+    const expensesData = {};
+    expenses.forEach(({ id, description, note, amount, createdAt }) => {
+        expensesData[id] = { description, note, amount,createdAt };
+    });
+    database.ref('expenses').set(expensesData).then(() => done());
+});
 
 test('Should setup remove expense action object', () => {
     const action = removeExpense({ id: '123abc' });
@@ -96,4 +104,12 @@ test('Should add expense with defaults to database and store', (done) => {
         });
         done();
     });
+});
+
+test('Should setup set expenses action object with data', () => {
+    const action = setExpenses(expenses);
+    expect(action).toEqual({
+        type: 'SET_EXPENSES',
+        expenses
+    })
 });
