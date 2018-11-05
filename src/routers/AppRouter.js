9d38cf241880e16094ef5f5055b37ba65987e7ef
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-11-05 08:40:55 -0800

    edited the router \ routing to login at route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 
 /* PAGES */
+import { LoginPage } from '../components/LoginPage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
@@ -17,7 +18,8 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/" component={LoginPage} exact={true} />
+                <Route path="/dashboard" component={ExpenseDashboardPage} exact={true} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
