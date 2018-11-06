c55b8142bd7c9329a83cc571dce3c204b3563826
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-11-06 08:27:23 -0800

    coded logout functionalities with test cases

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 describe('Header Component', () => {
+    let startLogout, wrapper;
+
+    beforeEach(() => {
+        startLogout = jest.fn();
+        wrapper = shallow(<Header startLogout={startLogout} />);
+    });
+
     test('Should render header correctlly', () => {
-        const wrapper = shallow(<Header />);
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Should call start logout on click', () => {
+        wrapper.find('button').simulate('click');
+        expect(startLogout).toHaveBeenCalled();
     });
 });
